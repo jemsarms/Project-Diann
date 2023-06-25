d5e3032a7be86de2f95a5b02de97d7c32eff700c
jermainejem4@gmail.com <jermainejem4@gmail.com>
2023-06-25 16:43:12 +0800

    Fixes to index

--- src/index.js
+++ src/index.js
@@ -1,6 +1,1 @@
-if (window.location.href.includes("index.html")) {
-  document.getElementById("nextPage").addEventListener("click", () => {
-    window.location.href = "first-page.html";
-  });
-}
 
